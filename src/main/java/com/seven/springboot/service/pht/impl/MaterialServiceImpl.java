62cf59cced9d6825f4bb0932c20a13638fa44a08
刘远宏 <1728381749@qq.com>
2021-04-04 20:31:14 +0800

    LYH第一次提交

--- src/main/java/com/seven/springboot/service/pht/impl/MaterialServiceImpl.java
+++ src/main/java/com/seven/springboot/service/pht/impl/MaterialServiceImpl.java
@@ -3,6 +3,7 @@ package com.seven.springboot.service.pht.impl;
 import com.seven.springboot.mapper.pht.MaterialDao;
 import com.seven.springboot.pojo.TPjMaterialContractDetailed;
 import com.seven.springboot.service.pht.MaterialService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.Resource;
@@ -10,6 +11,7 @@ import java.util.List;
 
 @Service
 public class MaterialServiceImpl implements MaterialService {
+
 
     @Resource
     private MaterialDao materialDao;
