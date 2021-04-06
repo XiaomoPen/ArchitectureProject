42bfc31a54fdc696ead332d6db168af5c19a61fd
XiaomoPen <977033704@qq.com>
2021-04-06 17:40:14 +0800

    彭海涛：完成了材料总计划、材料总计划列表、材料需求计划、材料需求计划列表、材料合同、材料合同列表业务需求。

---
+++ src/main/java/com/seven/springboot/pojo/TCtLxr.java
@@ -0,0 +1,93 @@
+package com.seven.springboot.pojo;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.sql.Timestamp;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class TCtLxr {
+
+  private String lxrBh;
+  private String khxxBh;
+  private String lxrXm;
+  private String lxrDh;
+  private String lxrZw;
+  private String lxrYxl;
+  private String lxrZt;
+  private String khxxXm;
+
+
+  public String getLxrBh() {
+    return lxrBh;
+  }
+
+  public void setLxrBh(String lxrBh) {
+    this.lxrBh = lxrBh;
+  }
+
+
+  public String getKhxxBh() {
+    return khxxBh;
+  }
+
+  public void setKhxxBh(String khxxBh) {
+    this.khxxBh = khxxBh;
+  }
+
+
+  public String getLxrXm() {
+    return lxrXm;
+  }
+
+  public void setLxrXm(String lxrXm) {
+    this.lxrXm = lxrXm;
+  }
+
+
+  public String getLxrDh() {
+    return lxrDh;
+  }
+
+  public void setLxrDh(String lxrDh) {
+    this.lxrDh = lxrDh;
+  }
+
+
+  public String getLxrZw() {
+    return lxrZw;
+  }
+
+  public void setLxrZw(String lxrZw) {
+    this.lxrZw = lxrZw;
+  }
+
+
+  public String getLxrYxl() {
+    return lxrYxl;
+  }
+
+  public void setLxrYxl(String lxrYxl) {
+    this.lxrYxl = lxrYxl;
+  }
+
+
+  public String getLxrZt() {
+    return lxrZt;
+  }
+
+  public void setLxrZt(String lxrZt) {
+    this.lxrZt = lxrZt;
+  }
+
+
+  public String getKhxxXm() {
+    return khxxXm;
+  }
+
+  public void setKhxxXm(String khxxXm) {
+    this.khxxXm = khxxXm;
+  }
+
+}
